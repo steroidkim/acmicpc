c0c3cccaafb7fde05259c2df223bc659cb8e377e
minshu.kim <minshu.kim@lge.com>
2017-12-26 16:47:53 +0900

    hackerrank: gem stones
    https://www.hackerrank.com/challenges/gem-stones

---
+++ hackerrank/gem_stones.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int alphabet[26];
+int gemstones(vector <string> arr){
+    // Complete this function
+    int cnt = 0;
+
+     for (int i=0; i<arr.size(); i++) {
+        string str = arr[i];
+        for (int j=0; j<str.length(); j++) {
+            int index = str[j] - 'a';
+            alphabet[index]++;
+        }
+     }
+
+     for (int i=0; i<26; i++) {
+        if (alphabet[i] >= arr.size()) {
+            cnt++;
+        }
+     }
+
+     for (int i=0; i<26; i++)
+        cout << alphabet[i];
+
+     return cnt;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       cin >> arr[arr_i];
+    }
+    int result = gemstones(arr);
+    cout << "result: " << result << endl;
+    return 0;
+}
