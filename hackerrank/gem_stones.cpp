228418b1d135237b1534db1b8b79500c02a89716
minshu.kim <minshu.kim@lge.com>
2017-12-27 08:53:31 +0900

    hackerrank: gem stones#2
    https://www.hackerrank.com/challenges/gem-stones
    다음날에 생각나서 해결함. vector의 index를 활용하였음.

--- hackerrank/gem_stones.cpp
+++ hackerrank/gem_stones.cpp
@@ -3,26 +3,24 @@
 using namespace std;
 
 int alphabet[26];
+
 int gemstones(vector <string> arr){
     // Complete this function
     int cnt = 0;
 
-     for (int i=0; i<arr.size(); i++) {
+    for (int i=0; i<arr.size(); i++) {
         string str = arr[i];
         for (int j=0; j<str.length(); j++) {
             int index = str[j] - 'a';
-            alphabet[index]++;
+            if (alphabet[index] == i) alphabet[index]++;
         }
      }
 
      for (int i=0; i<26; i++) {
-        if (alphabet[i] >= arr.size()) {
+        if (alphabet[i] == arr.size()) {
             cnt++;
         }
      }
-
-     for (int i=0; i<26; i++)
-        cout << alphabet[i];
 
      return cnt;
 }
@@ -35,6 +33,6 @@ int main() {
        cin >> arr[arr_i];
     }
     int result = gemstones(arr);
-    cout << "result: " << result << endl;
+    cout << result << endl;
     return 0;
 }
